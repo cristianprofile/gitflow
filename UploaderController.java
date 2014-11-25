db3911479c3cacedd11f0094611266e4cc5d584e
cristianprofile <cristianprofile@gmail.com>
2014-11-25 19:06:18 +0100

    added new feature to work in group mode

--- UploaderController.java
+++ UploaderController.java
@@ -3,7 +3,7 @@ package com.mylab.cromero.web.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-
+sdadadadasdazxczxczc
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
